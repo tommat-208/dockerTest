8a9f0101e25441ac560ecb8b81c5acc336b35e4f
Tommaso <tommaso.matteoni@cho.it>
2025-01-16 10:16:59 +0100

    Add project files.

---
+++ dockerTest/Controllers/StringController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace dockerTest.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class StringController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetString()
+        {
+            return Ok("ciao sono l'api :D");
+        }
+
+        
+    }
+}
